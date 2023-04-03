f52ab648dee4d9566ee4a0a03f264f32322f4b08
sahanenes <sahanenesss@gmail.com>
2023-04-03 17:06:03 +0300

    server.js

--- client/script.js
+++ client/script.js
@@ -21,7 +21,7 @@ function typeText(element, text) {
   let index = 0;
   let interval = setInterval(() => {
     if (index < text.length) {
-      element.innerHtml += text.chartAt(index);
+      element.innerHtml += text.charAt(index);
       index++;
     } else {
       clearInterval();
@@ -70,6 +70,28 @@ const handleSubmit = async (e) => {
   const messageDiv = document.getElementById(uniqueId);
 
   loader(messageDiv);
+
+  const response = await fetch("http://localhost:5000", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({
+      prompt: data.get("prompt"),
+    }),
+  });
+  clearInterval(loadInterval);
+  messageDiv.innerHTML = "";
+
+  if (response.ok) {
+    const data = await response.json();
+    const parsedData = data.bot.trim();
+    typeText(messageDiv, parsedData);
+  } else {
+    const err = await response.text();
+    messageDiv.innerHTML = "Something went wrong";
+    alert(err);
+  }
 };
 
 form.addEventListener("submit", handleSubmit);
